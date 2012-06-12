80835d06d25f3ff9961e6a24ca3b0be9fde4c7d4
Daniel Himmelein <daniel.himmelein@googlemail.com>
2012-06-12 11:04:54 +0200

    Fixes

--- make.sh
+++ make.sh
@@ -1,2 +1,12 @@
 . ./envsetup.sh
+cd ..
+cd rootfs/lib
+ln -s libgcc_s.so.1 libgcc_s.so
+cd ..
+cd usr/lib
+ln -s libstdc++.so.6.0.13 libstdc++.so
+cd ..
+cd ..
+cd ..
+cd AndroidTransporterPlayer
 make
