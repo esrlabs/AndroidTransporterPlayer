b67edc9a7282e48385f984b5568d731c7bbdc473
Christian Köstlin <christian.koestlin@esrlabs.com>
2012-07-25 14:43:36 +0200

    adapted to mindroid
    Change-Id: Ibafa5cbd829f939572391d64c8e85be5f629c9ba

--- project.rb
+++ project.rb
@@ -2,5 +2,5 @@ cxx_configuration do
   exe 'AndroidTransporterPlayer',
     :sources => FileList['*.cpp'],
     :includes => ['.'],
-    :dependencies => ['android-os'] + bin_libs('ilclient', 'GLESv2', 'EGL', 'openmaxil', 'bcm_host','vcos', 'vchiq_arm')
+    :dependencies => ['mindroid'] + bin_libs('ilclient', 'GLESv2', 'EGL', 'openmaxil', 'bcm_host', 'vcos', 'vchiq_arm')
 end
