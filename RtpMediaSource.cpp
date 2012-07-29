d82aa86ba71cc3ed6e2b3043c83d8821349c1407
Daniel Himmelein <daniel.himmelein@gmail.com>
2012-07-29 17:24:53 +0200

    fixed license header
    Change-Id: Ie90f41a4b286a2d326dd798e03cfce5e211c077d

--- RtpMediaSource.cpp
+++ RtpMediaSource.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 E.S.R.Labs GmbH
+ * Copyright (C) 2010 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Refactorings by E.S.R.Labs GmbH
  */
 
 #include "RtpMediaSource.h"
