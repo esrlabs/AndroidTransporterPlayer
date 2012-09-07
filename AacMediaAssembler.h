4d9aa97b89387b934fd895bc226d9d3edb6c0df6
Daniel Himmelein <daniel.himmelein@gmail.com>
2012-09-07 16:38:05 +0200

    license headers
    Change-Id: Icb13198639d02cc31336cd1be39ac07d069a615f

--- AacMediaAssembler.h
+++ AacMediaAssembler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2012 E.S.R.Labs GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,8 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Refactorings by E.S.R.Labs GmbH
  */
 
 #ifndef AACMEDIAASSEMBLER_H_
