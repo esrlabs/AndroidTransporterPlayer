b8bda236ed8e5efb959001e248b36daf36aef4ca
Christian Köstlin <christian.koestlin@esrlabs.com>
2012-09-10 17:45:31 +0200

    cleaned up cxx-project ... please update cxx installation
    Change-Id: Ie1e84543bdda4bd5ee017b30f2227091fd6607cb

--- project.rb
+++ project.rb
@@ -1,6 +1,23 @@
 cxx_configuration do
+  bin_libs('GLESv2', 'EGL', 'openmaxil', 'vchiq_arm')
+  bin_lib('ilclient',
+          {
+            :includes => ["#{RASP_ROOTFS}/opt/vc/src/hello_pi/libs/ilclient"],
+            :lib_path => "#{RASP_ROOTFS}/opt/vc/src/hello_pi/libs/ilclient"
+          })
+  bin_lib('bcm_host',
+          {
+            :includes => ["#{RASP_ROOTFS}/opt/vc/include"],
+            :lib_path => "#{RASP_ROOTFS}/opt/vc/lib"
+          })
+  bin_lib('vcos',
+          {
+            :includes => ["#{RASP_ROOTFS}/opt/vc/include/interface/vcos/pthreads"],
+            :lib_path => "#{RASP_ROOTFS}/opt/vc/lib"
+          })
+
   exe 'AndroidTransporterPlayer',
     :sources => FileList['*.cpp','rtcp/**/*.cpp'],
     :includes => ['.'],
-    :dependencies => ['mindroid', 'fdk-aac'] + bin_libs('ilclient', 'GLESv2', 'EGL', 'openmaxil', 'bcm_host', 'vcos', 'vchiq_arm')
+    :dependencies => ['mindroid', 'fdk-aac', 'ilclient', 'GLESv2', 'EGL', 'openmaxil', 'bcm_host', 'vcos', 'vchiq_arm']
 end
